9f95bd793eb84b1c0301f44f8c98da396bde949b
Scott Rouse <scott.rouse@gmail.com>
2023-01-08 11:23:25 -0600

    Updated file: "transformations/fdst-web/fds-icon.js"

--- transformations/fdst-web/fds-icon.js
+++ transformations/fdst-web/fds-icon.js
@@ -39,13 +39,16 @@ const template = document.createElement('template');
     async getSvg(icon) {
       let svg;
       switch (icon) {
-                case 'default' :
+        case 'default' :
           svg = await import('./icons/default.js');
-          break;        case 'frog' :
+          break;
+        case 'frog' :
           svg = await import('./icons/frog.js');
-          break;        case 'cube' :
+          break;
+        case 'cube' :
           svg = await import('./icons/cube.js');
-          break;        case 'default' :
+          break;
+        case 'default' :
           svg = await import('./icons/default.js');
           break;
       }
